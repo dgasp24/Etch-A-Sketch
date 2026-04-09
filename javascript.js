57ab4c9e6b47d901effd09cc8da6b38903068537
dgasp24 <dommygabby7@gmail.com>
2026-04-09 01:11:31 -0500

    Refactored layout and styling for user input and grid display; enhanced input validation feedback

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const container = document.querySelector("#container");
 const userSelect = document.querySelector("#userSelection");
-const announcement = document.createElement("p");
+const left = document.querySelector("#left");
 
 function getRandom(){
     let number = Math.floor(Math.random() * 256);
@@ -8,27 +8,38 @@ function getRandom(){
 }
 
 function reset(){
+    const gridInfo = document.querySelector("#info");
+
+    if(gridInfo){
+        gridInfo.remove();
+    }
     container.textContent = "";
-    createGrid(20);
+    createGrid(16);
 }
 
 function grabValue(){
-    const input = document.querySelector("#input");
+    const input = document.querySelector("#input"); //Input Box
+    let gridInfo = document.querySelector("#info");
     let value = +input.value;
 
-    const oldMsg = document.querySelector(".error");
-    if(oldMsg){
-        oldMsg.remove();
+    if(!gridInfo){ //If gridInfo doesn't exist, create one
+        gridInfo = document.createElement("div");
+        gridInfo.setAttribute("id", "info");
+        gridInfo.classList.add("left-side");
+        left.appendChild(gridInfo);
     }
 
+    //Resets div so no repeats
+     gridInfo.textContent = "";
+
+    //Creates text content in gridInfo where it tells grid size or if theres an invalid input
     if(value < 16 || value > 100){
-        announcement.textContent = "Invalid Input, please put a number between 16 and 100!";
-        announcement.classList.add("error");
-        userSelect.appendChild(announcement);
+        gridInfo.style.fontSize = "18px";
+        gridInfo.textContent = "Invalid input, please put a number between 16 and 100!";
         return;
     }else{
-        announcement.textContent = `${value}x${value}`;
-        userSelect.appendChild(announcement);
+        gridInfo.style.fontSize = "60px";
+        gridInfo.textContent = `${value}x${value}`;
         createGrid(value);
     }
 }
@@ -39,7 +50,7 @@ function createGrid(size){
     for(let i = 1; i <= size; i++){
         for(let j =1; j <= size; j++){
         const divs = document.createElement("div");
-            divs.setAttribute("style", `width: calc(100%/ ${size})`)
+            divs.setAttribute("style", `width: calc(100%/ ${size}); background-color: white`)
             divs.setAttribute("class", "divs");
             container.appendChild(divs);
 
